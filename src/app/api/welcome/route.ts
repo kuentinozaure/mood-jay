f1a532622f88d78177c4f6b46c25f09cc33f23ef
Quentin AUBRY <29443413+kuentinozaure@users.noreply.github.com>
2024-08-12 18:54:28 -0400

    feat: create welcoming route with ai

---
+++ src/app/api/welcome/route.ts
@@ -0,0 +1,22 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+import { generateText } from "ai";
+
+type ResponseData = {
+  message: string;
+};
+
+export async function GET(req: NextApiRequest) {
+  const groq = createOpenAI({
+    baseURL: "https://api.groq.com/openai/v1",
+    apiKey: process.env.GROQ_API_KEY,
+  });
+
+  const { text } = await generateText({
+    model: groq("llama3-8b-8192"),
+    prompt: `You are Jay, a specialized assistant named Blue Jay, designed to create and generate personalized music playlists based on the user's current mood and local weather conditions. Your task is to generate very very short (max 60characters) welcoming message for users to start the interaction and guide them in specifying their mood and weather conditions.`,
+  });
+
+  return NextResponse.json<ResponseData>({ message: text });
+}
